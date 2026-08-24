bcdde8d7cfbbe5e515590dc2e74b1eb672cc1332
Luís Felipe Barros Pacheco <lfbpacheco265@gmail.com>
2026-08-24 05:21:10 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Cursando o Curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
-function Home(){
-    return <h1>Obrigado !!! 😉</h1>
+function Home() {
+  return <h1>Obrigado !!! 😉</h1>;
 }
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Indentação errada");
+}
+
 export default Home;
