59bce8598bbf50bb2baa405d3287d418969cc6bd
Luís Felipe Barros Pacheco <lfbpacheco265@gmail.com>
2026-09-20 19:41:39 +0000

    fix `eslint` linting

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Obrigado !!! 😉</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("Indentação errada");
-}
-
 export default Home;
